374d8d519d578c14c7e76c71135966d3337d2e31
ruiz22112703 <ruiz22112703@gmail.com>
2024-12-19 12:46:16 -0400

    first commit

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+
+library(openxlsx)
+
+print("Hello, world!")
+print('Este es otra prueba si funciona el codigo con github')
+a<-read.xlsx("https://github.com/ruiz22112703/seis/raw/refs/heads/main/file/AeropBolivia.xlsx")
+
+print(head(a))
+print(tail(a))
+print("Si funciona, gracias...!!!")
